25a044a972bce8f45028840c831d47280a6ec965
timH <thanneman@csumb.edu>
2020-03-30 21:20:42 -0700

    Oh No! Now i'm Committed

---
+++ Http_Server.py
@@ -0,0 +1,117 @@
+#!/bin/python3
+'''
+This is an extention of Server_Socks that implements an http server
+@Author Tim Hanneman
+@Date MAR 20 2020
+@License Copyright Tim Hanneman
+'''
+#@TODO: Clean-up logging events, and write them to standard error, put standard error into a logfile.
+#@TODO: Thread the server...way to slow like this
+#@TODO: Build an index of files it will serve. This will help prevent directory walking.
+
+from Server_Socks import Server_Socks
+import os
+
+class Http_Server(Server_Socks):
+
+    def __init__(self, runOptions=False, addr='127.0.0.1', port=8080):
+        self.launch_options(runOptions, addr, port)
+        self.create_http_server()
+
+    #@Todo:
+    def sanitize_request(self, line):
+        pass
+    def get_request(self):
+        pass
+    def head_request(self):
+        pass
+    def post_request(self):
+        pass
+
+
+        return uri
+        
+    def create_http_server(self):
+        c = self.create_Server()
+        while 1:
+            try:
+                c_socket, c_address = c.accept()
+                line_in = c_socket.recv(1024).decode('utf-8')
+                print(c_address, end=" ")
+                #cfile = c_socket.makefile('rw', 1024)
+                #line = cfile.readline().strip()
+                request = line_in.strip().split(' ')
+                print("Request: " + str(request[:3]))
+                status_code = request[0]
+                uri_get = request[1]
+                proto_ver = request[2]
+
+                #print(status_code + " " + uri_get + " " + proto_ver + "Is this spliting properly?")
+           
+            except:
+                print(" An error has occurred in processing the request")
+                ##cfile.close()
+                c_socket.close()
+                continue
+           
+            if status_code == 'GET':
+                #print(uri_get + " uri_get")
+                if uri_get == '/':
+                    uri_get='./index.html'
+
+                else:
+                    try:
+                        uri_get = uri_get.split('?')[0]
+                        uri_get = './' + str(uri_get).lstrip('/')
+                        #print(uri_get + "  uri_get value")
+                        #could possibly check for an empty string here in uri_get to set the default page
+                    except:
+                        print('Processing the uri failed')
+                        continue
+            else:
+                #cfile.write('HTTP/1.0 501 Not Implemented')
+                #cfile.close()
+                c_socket.send('HTTP/1.0 501 Not Implemented '.encode('utf-8'))
+                c_socket.close()
+                continue
+
+            try:
+                #print(uri_get)
+                get_file = open(str(uri_get), 'rb')
+                response = get_file.read()
+                get_file.close()
+
+                if(uri_get.endswith(".jpg")):
+                    mimetype = 'image/jpg'
+                elif(uri_get.endswith(".css")):
+                    mimetype = 'text/css'
+                elif(uri_get.endswith(".png")):
+                    mimetype = 'image/png'
+                elif(uri_get.endswith(".svg")):
+                    mimetype = 'image/svg'
+                elif(uri_get.endswith(".gif")):
+                    mimetype = 'image/gif'
+                elif(uri_get.endswith(".midi")):
+                    mimetype = 'audio/sp-midi'
+                elif(uri_get.endswith(".mp3")):
+                    mimetype = 'audio/mpeg'
+                elif(uri_get.endswith(".ogg")):
+                    mimetype = 'audio/ogg'
+                else:
+                    mimetype = 'text/html'
+
+                header = 'HTTP/1.0 200 OK\n' + 'Content-Type: ' + str(mimetype) + '\n\n'
+                final_response = header.encode('utf-8') + response
+                c_socket.send(final_response)
+                c_socket.close()
+
+            except:
+                print(' Reading and sending the uri failed')
+                continue
+         
+
+         
+        c_socket.close()
+
+if __name__=='__main__':
+    sock = Http_Server(True)
---
+++ Server_Socks.py
@@ -0,0 +1,130 @@
+#!/bin/bash/python3
+'''
+This is a simple webserver based on the sockets library
+@Author Tim Hanneman
+@Date MAR 19 2020
+@License Copyright Tim Hanneman
+
+@TODO: Split the createServer code into the creation of a socket and the type of server. Make a subclasses that define the server type.
+@TODO: The default, acceptance and sending of objects needs improvement, seems to randomly fail sometimes
+@TODO: Add a way for the server to be shutdown gracefully
+@TODO: Sanitize web requests better for invalid input.
+@TODO needs to be threaded, and test out how it fails.
+@TODO: Can I chroot jail it?
+@TODO: Get commandline arguments for setting ip and port
+#DONE? Security audit, make sure it can only access files within it's directory.
+#Seems like it is pretty resistant to accessing other parts of the hd. There might be ways around that though.
+'''
+
+import socket
+import os
+
+class Server_Socks:
+  'Defines a port and address to setup a server'
+   
+  __host_addr = '127.0.0.1'
+  __host_port = 8080
+  __ASSET_DIRECTORY = 'WWW'
+
+  #@TODO: add parameters for add and port??? 
+  def __init__(self, runOptions=False, addr='127.0.0.1', port=8080):
+    self.launch_options(runOptions, addr, port)
+
+  def _set_Working_Directory(self):
+    try:
+      __cwd = './' + self.__ASSET_DIRECTORY
+      print('Changing working directory to' + __cwd)
+      os.chdir(__cwd)
+      return __cwd
+    except:
+      print('Setting the working directory failed. Is this Windows?')
+      __cwd = '.\\' + self.__ASSET_DIRECTORY
+      os.chdir(__cwd)
+      return __cwd
+
+  def set_Addr(self, addr):
+    self.__host_addr = addr
+    print("setting address to : " + str(self.__host_addr))
+
+  def get_Addr(self):
+    return self.__host_addr
+   
+  def set_Port(self, port):
+    self.__host_port = port
+    print("setting the port to: " + str(self.__host_port))
+
+  def get_Port(self):
+    return self.__host_port
+
+  def set_Both(self, addr, port):
+    self.set_Addr(addr)
+    self.set_Port(port)
+
+  def launch_options(self, runOptions=False, addr='127.0.0.1', port=8080):
+    if(runOptions == False):
+      #Begin by searching for a configuration file
+      try:
+        for root, dirs, files in os.walk(os.getcwd()):
+          if '.config' in files:
+            print('Starting from configuration file: ')
+            cfg = open('.config')
+            addr, port = cfg.readline().split()
+            self.set_Both(addr, int(port))
+            cfg.close()
+            self._set_Working_Directory()
+            return
+
+      except:
+        print('Read from config failed')
+
+    elif runOptions:
+      print("Enter IP (v4) and port to listen on, or try to auto detect with auto: \n>addr port")
+      cli = input()
+       
+      if cli == 'auto':
+        try:
+          addr = socket.gethostbyname(socket.gethostname())
+          self.set_Both(addr, 8080)
+          print('IP to use is:' + self.get_Addr() + " and the port will be " + str(self.get_Port()))
+          self._set_Working_Directory()
+          return
+
+        except:
+          print('auto-config failed')
+          print("Enter IP (v4) and port to listen on, or try to auto detect with auto: \n>addr port")
+          cli = input()
+      else:
+        try:
+          addr, port = cli.split()
+          self.set_Both(addr,int(port))
+          self._set_Working_Directory()
+          return
+
+        except:
+          print('invalid input')
+
+    self.set_Both(addr,port)
+
+  def create_Server(self):
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    c.bind((self.get_Addr(),self.get_Port()))
+    c.listen(1)
+    return c
+
+#def main(argv):
+#  try:
+#    opts, args = getopt.getopt(argv,"i" ["addr=","port="])
+#  except getopt.GetoptError:
+#    print("Options are:")
+#    sys.exit(2)
+        
+if __name__ == '__main__':
+#  import sys, getopt
+#  main(sys.argv[1:])
+#if((len(sys.argv)>0) and (len(sys.argv)<4)):
+#arg_List = str(sys.argv).split()
+#server = Server_Socks(bool(arg_List[0]),arg_List[1],int(arg_List[2]))
+
+  server = Server_Socks(True)
+  server.create_Server
